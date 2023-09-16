cd7bc0df26ba1654d8c31187014cadc56d1d96e5
jesus <jesusromanm99@fpuna.edu.py>
2023-09-15 22:32:21 -0400

    Route

--- src/components/RootRoutes.tsx
+++ src/components/RootRoutes.tsx
@@ -1,9 +1,14 @@
 import { Routes, Route } from "react-router-dom";
 import LoginPage from "../pages/user/LoginPage";
+//import ListUserPage from "../pages/user/ListUserPage";
+import CreateUserPage from "../pages/user/CreateUserPage";
+import ListUserPage from "../pages/user/ListUserPage";
 function RootRoutes() {
   return (
     <Routes>
       <Route path='/login' element={<LoginPage />} />
+      <Route path='/sign-up' element={<CreateUserPage />} />
+      <Route path='/users' element={<ListUserPage />} />
     </Routes>
   );
 }

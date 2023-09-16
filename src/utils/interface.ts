f56ced45946cbadde4b35e1784670600e9fba058
jesus <jesusromanm99@fpuna.edu.py>
2023-09-15 22:32:05 -0400

    interface

--- src/utils/interface.ts
+++ src/utils/interface.ts
@@ -2,3 +2,28 @@ export interface RLogin {
   username: string;
   password: string;
 }
+
+export interface User {
+  username: string;
+  password: string;
+  email: string;
+  isStaff: boolean;
+  isSuperUser: boolean;
+}
+
+export interface Categoria {
+  id: number;
+  name: string;
+}
+
+export interface Imagen {
+  id: number;
+  url: string;
+}
+
+export interface Product {
+  Nombre: string;
+  Estado: string;
+  Categorías: Categoria[];
+  Imágenes: Imagen[];
+}
